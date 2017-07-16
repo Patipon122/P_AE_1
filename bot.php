ce36f64c473f0c019ae57d70e38f439c39d8f86b
Patipon122 <patipon.ae@hotmail.com>
2017-07-16 20:17:37 +0700

    yyy

--- bot.php
+++ bot.php
@@ -1,38 +1,38 @@
-php
+<?php
 $access_token = 'z0S/egyN8VF0kBchfpDsIhw7rhPhrdLrI23elhWabjKHFqUOemBpxB95AJyFN2yOBUYUGRDwo5eM/cWje44uSR4x17xBvlvL1kfwsK808eXTIakT5jVUc0hFqXRLkey+rCge/8bJPEfZqnHYgtXI2QdB04t89/1O/w1cDnyilFU=';
 
- Get POST body content
-$content = file_get_contents('phpinput');
- Parse JSON
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
 $events = json_decode($content, true);
- Validate parsed JSON data
+// Validate parsed JSON data
 if (!is_null($events['events'])) {
-	 Loop through each event
+	// Loop through each event
 	foreach ($events['events'] as $event) {
-		 Reply only when message sent is in 'text' format
+		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			 Get text sent
+			// Get text sent
 			$text = $event['message']['text'];
-			 Get replyToken
+			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			 Build message to reply back
+			// Build message to reply back
 			$messages = [
-				'type' = 'text',
-				'text' = $text
+				'type' => 'text',
+				'text' => $text
 			];
 
-			 Make a POST Request to Messaging API to reply to sender
-			$url = 'httpsapi.line.mev2botmessagereply';
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'replyToken' = $replyToken,
-				'messages' = [$messages],
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
-			$headers = array('Content-Type applicationjson', 'Authorization Bearer ' . $access_token);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, POST);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -40,8 +40,8 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . rn;
+			echo $result . "\r\n";
 		}
 	}
 }
-echo OK;
+echo "OK";
